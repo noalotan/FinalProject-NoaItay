309233b47e711fde374ff9b095c0896d3f233373
noalotan <noalotan1@gmail.com>
2024-10-17 13:26:04 +0300

    Create elasticache_redis.tf

---
+++ eks-terraform/elasticache_redis.tf
@@ -0,0 +1,14 @@
+resource "aws_elasticache_subnet_group" "redis_subnet_group" {
+  name       = var.redis_subnet_group_name
+  subnet_ids = module.vpc.private_subnets
+}
+
+resource "aws_elasticache_cluster" "redis" {
+  cluster_id           = var.redis_cluster_id
+  engine               = "redis"
+  node_type            = "cache.t2.micro"
+  num_cache_nodes      = 1
+  parameter_group_name = "default.redis7"
+  subnet_group_name    = aws_elasticache_subnet_group.redis_subnet_group.name
+  tags                 = local.billing_tags
+}
