1b6a630078e855b1b9464451f67d0bed6ec5980a
noalotan <noalotan1@gmail.com>
2024-10-17 13:26:18 +0300

    Update variables.tf

--- eks-terraform/variables.tf
+++ eks-terraform/variables.tf
@@ -1,3 +1,5 @@
+# general vars
+
 variable "region" {
   description = "AWS region"
   type        = string
@@ -16,4 +18,46 @@ variable "billing_tags" {
   default     = {
     Project = "TeamA"
   }
+}
+
+
+
+
+
+# postgresql db vars
+
+variable "db_name" {
+  description = "The name of the PostgreSQL database instance"
+  default     = "itayandnoa"
+}
+
+variable "db_user" {
+  description = "The database admin user name"
+  default     = "itayandnoa"
+}
+
+variable "db_password" {
+  description = "The database admin password"
+  default     = "itayandnoa"
+}
+
+variable "db_subnet_group_name" {
+  description = "The name of the DB subnet group"
+  default     = "noa-itay-db-subnet-group"
+}
+
+
+
+
+
+# redis vars
+
+variable "redis_cluster_id" {
+  description = "The ID of the ElastiCache Redis cluster"
+  default     = "noa-itay-redis-cache"
+}
+
+variable "redis_subnet_group_name" {
+  description = "The name of the ElastiCache subnet group"
+  default     = "noa-itay-redis-subnet-group"
 }
