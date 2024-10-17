b77794331fced7d4ba1aa10ed26ecc5e4f22b3f6
noalotan <noalotan1@gmail.com>
2024-10-17 11:06:30 +0300

    Update outputs.tf

--- eks-terraform/outputs.tf
+++ eks-terraform/outputs.tf
@@ -1,19 +1,34 @@
-output "cluster_endpoint" {
-  description = "Endpoint for EKS control plane"
+output "vpc_id" {
+  description = "The ID of the created VPC"
+  value       = module.vpc.vpc_id
+}
+
+output "public_subnet_ids" {
+  description = "The IDs of the public subnets"
+  value       = module.vpc.public_subnets
+}
+
+output "private_subnet_ids" {
+  description = "The IDs of the private subnets"
+  value       = module.vpc.private_subnets
+}
+
+output "eks_cluster_name" {
+  description = "The name of the EKS cluster"
+  value       = module.eks.cluster_id
+}
+
+output "eks_cluster_endpoint" {
+  description = "The endpoint of the EKS cluster"
   value       = module.eks.cluster_endpoint
 }
 
-output "cluster_security_group_id" {
-  description = "Security group ids attached to the cluster control plane"
-  value       = module.eks.cluster_security_group_id
+output "eks_security_group_id" {
+  description = "The security group ID for EKS nodes"
+  value       = aws_security_group.eks_nodes.id
 }
 
-output "region" {
-  description = "AWS region"
-  value       = var.region
-}
-
-output "cluster_name" {
-  description = "Kubernetes Cluster Name"
-  value       = module.eks.cluster_name
-}
+# output "nat_gateway_id" {
+#  description = "The ID of the NAT Gateway (if enabled)"
+#  value       = module.vpc.enable_nat_gateway ? module.vpc.nat_gateway_id : null
+#}
