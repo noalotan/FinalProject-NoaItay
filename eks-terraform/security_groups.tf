319924dae8c1d28a26cf0e7b9fd269bcc470617e
noalotan <noalotan1@gmail.com>
2024-10-22 12:40:54 +0300

    Update security_groups.tf

--- eks-terraform/security_groups.tf
+++ eks-terraform/security_groups.tf
@@ -1,25 +1,3 @@
-resource "aws_security_group" "eks_nodes" {
-  name        = "eks-sg-${local.resource_name}"
-  description = "Allow traffic to EKS nodes"
-  vpc_id      = module.vpc.vpc_id
-
-  ingress {
-    from_port   = 0
-    to_port     = 65535
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"  # All traffic
-    cidr_blocks = ["0.0.0.0/0"] 
-  }
-
-  tags = local.billing_tags
-}
-
 resource "aws_security_group" "elasticache_sg" {
   name        = "elasticache-sg-${local.resource_name}"
   description = "Security group for ElastiCache"
