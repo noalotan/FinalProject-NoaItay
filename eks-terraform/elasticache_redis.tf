6974964418a448cf354a758315dfe940b41e5142
itayshlanger <itayshlanger16@gmail.com>
2024-10-22 13:28:00 +0300

    Update elasticache_redis.tf

--- eks-terraform/elasticache_redis.tf
+++ eks-terraform/elasticache_redis.tf
@@ -7,7 +7,8 @@ resource "aws_elasticache_replication_group" "redis" {
   replication_group_id          = var.redis_replication_group_id
   description                   = "Redis replication group"
   node_type                     = "cache.t2.micro"
-  number_cache_nodes            = 1  
+  num_node_groups               = 1
+  replicas_per_node_groups      = 0
   parameter_group_name          = "default.redis7"
   subnet_group_name             = aws_elasticache_subnet_group.redis_subnet_group.name
   security_group_ids            = [aws_security_group.elasticache_sg.id]
