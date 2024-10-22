e428b1f5b794a851251c15f98b9adb7cb8b6d770
noalotan <noalotan1@gmail.com>
2024-10-22 12:39:09 +0300

    Update rds-postgresql.tf

--- eks-terraform/rds-postgresql.tf
+++ eks-terraform/rds-postgresql.tf
@@ -14,5 +14,6 @@ resource "aws_db_instance" "mydb" {
   parameter_group_name = "default.postgres16"
   db_subnet_group_name = aws_db_subnet_group.mydb_subnet_group.name
   skip_final_snapshot  = true
+  vpc_security_group_ids = [aws_security_group.rds_sg.id]
   tags                 = local.billing_tags
 }
