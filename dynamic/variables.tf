0d4d1a2e895bdc3dba4854c15186044f12bac115
noalotan <noalotan1@gmail.com>
2024-10-23 12:44:23 +0300

    Update variables.tf

--- dynamic/variables.tf
+++ dynamic/variables.tf
@@ -21,35 +21,6 @@ variable "billing_tags" {
 }
 
 
-
-
-
-# postgresql db vars
-
-variable "db_name" {
-  description = "The name of the PostgreSQL database instance"
-  default     = "itayandnoa"
-}
-
-variable "db_user" {
-  description = "The database admin user name"
-  default     = "statuspage"
-}
-
-variable "db_password" {
-  description = "The database admin password"
-  default     = "itayandnoa"
-}
-
-variable "db_subnet_group_name" {
-  description = "The name of the DB subnet group"
-  default     = "noa-itay-db-subnet-group"
-}
-
-
-
-
-
 # redis vars
 
 variable "redis_replication_group_id" {
