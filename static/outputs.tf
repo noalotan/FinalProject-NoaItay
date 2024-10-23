9d8ca5f43b5e38a2f19e19c1f1ab731a04a8bcc7
noalotan <noalotan1@gmail.com>
2024-10-23 13:35:54 +0300

    Update outputs.tf

--- static/outputs.tf
+++ static/outputs.tf
@@ -54,3 +54,17 @@ output "db_instance_arn" {
   value       = aws_db_instance.mydb.arn
 }
 
+
+
+# security groups 
+
+output "elasticache_security_group_id" {
+  description = "The ID of the ElastiCache security group"
+  value       = aws_security_group.elasticache_sg.id
+}
+
+output "rds_security_group_id" {
+  description = "The ID of the RDS security group"
+  value       = aws_security_group.rds_sg.id
+}
+
