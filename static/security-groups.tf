1d86100b59254994b505d9e201f2e76d0cad674b
noalotan <noalotan1@gmail.com>
2024-10-23 13:35:04 +0300

    Create security-groups.tf

---
+++ static/security-groups.tf
@@ -0,0 +1,43 @@
+resource "aws_security_group" "elasticache_sg" {
+  name        = "elasticache-sg-${local.resource_name}"
+  description = "Security group for ElastiCache"
+  vpc_id      = module.vpc.vpc_id
+
+  ingress {
+    from_port   = 6379
+    to_port     = 6379
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]  # Adjust to your needs
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = local.billing_tags
+}
+
+resource "aws_security_group" "rds_sg" {
+  name        = "rds-sg-${local.resource_name}"
+  description = "Security group for RDS"
+  vpc_id      = module.vpc.vpc_id
+
+  ingress {
+    from_port   = 5432
+    to_port     = 5432
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]  # Adjust to your needs
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = local.billing_tags
+}
