9abb707c68de61c533db63acc1b7f0aba63c2aef
noalotan <noalotan1@gmail.com>
2024-10-23 12:44:40 +0300

    Update variables.tf

--- static/variables.tf
+++ static/variables.tf
@@ -19,3 +19,26 @@ variable "billing_tags" {
     Project = "TeamA"
   }
 }
+
+
+# postgresql db vars
+
+variable "db_name" {
+  description = "The name of the PostgreSQL database instance"
+  default     = "itayandnoa"
+}
+
+variable "db_user" {
+  description = "The database admin user name"
+  default     = "statuspage"
+}
+
+variable "db_password" {
+  description = "The database admin password"
+  default     = "itayandnoa"
+}
+
+variable "db_subnet_group_name" {
+  description = "The name of the DB subnet group"
+  default     = "noa-itay-db-subnet-group"
+}
