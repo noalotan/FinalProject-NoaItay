01c72af240a9d9692ac60be3a5b967e1572bdcbd
noalotan <noalotan1@gmail.com>
2024-10-23 12:46:52 +0300

    Create main.tf

---
+++ dynamic/main.tf
@@ -0,0 +1,15 @@
+provider "aws" {
+  region = var.region
+}
+
+data "aws_availability_zones" "available" {
+  filter {
+    name   = "opt-in-status"
+    values = ["opt-in-not-required"]
+  }
+}
+
+locals {
+  resource_name = var.resource_name
+  billing_tags = var.billing_tags
+}
