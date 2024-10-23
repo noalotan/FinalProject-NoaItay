8c63f51284a3b83f0eac14be684bd4818ea5d010
noalotan <noalotan1@gmail.com>
2024-10-23 13:24:15 +0300

    Update eks.tf

--- dynamic/eks.tf
+++ dynamic/eks.tf
@@ -8,7 +8,7 @@ module "eks" {
   cluster_endpoint_public_access           = true
   enable_cluster_creator_admin_permissions = true
 
-  vpc_id     = module.vpc.vpc_id
+  vpc_id     = data.terraform_remote_state.static.outputs.vpc_id
   subnet_ids = module.vpc.private_subnets
 
   eks_managed_node_group_defaults = {
