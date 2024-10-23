964cd54a569426234d309770918937b782a8dedb
noalotan <noalotan1@gmail.com>
2024-10-23 13:31:51 +0300

    Update outputs.tf

--- static/outputs.tf
+++ static/outputs.tf
@@ -1,3 +1,5 @@
+# vpc outputs
+
 output "vpc_id" {
   description = "The ID of the VPC"
   value       = module.vpc.vpc_id
@@ -11,4 +13,20 @@ output "public_subnets" {
 output "private_subnets" {
   description = "List of private subnet IDs"
   value       = module.vpc.private_subnets
+}
+
+
+
+
+
+# roles outputs
+
+output "eks_cluster_role_arn" {
+  description = "The ARN of the EKS cluster IAM role"
+  value       = aws_iam_role.eks_cluster_role.arn
+}
+
+output "eks_node_role_arn" {
+  description = "The ARN of the EKS node IAM role"
+  value       = aws_iam_role.eks_node_role.arn
 }
