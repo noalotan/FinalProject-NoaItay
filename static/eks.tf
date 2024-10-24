d10206e25de4e649e6f06cd7b2753ed075b947dd
itayshlanger <itayshlanger16@gmail.com>
2024-10-24 13:00:40 +0300

    Update eks.tf

--- static/eks.tf
+++ static/eks.tf
@@ -43,16 +43,16 @@ module "eks" {
 # Add-ons for EKS cluster
 
 resource "aws_eks_addon" "kube_proxy" {
-  cluster_name = module.eks.cluster_id
+  cluster_name = module.eks.cluster_name
   addon_name   = "kube-proxy"
 }
 
 resource "aws_eks_addon" "vpc_cni" {
-  cluster_name = module.eks.cluster_id
+  cluster_name = module.eks.cluster_name
   addon_name   = "vpc-cni"
 }
 
 resource "aws_eks_addon" "coredns" {
-  cluster_name = module.eks.cluster_id
+  cluster_name = module.eks.cluster_name
   addon_name   = "coredns"
 }
