05d4869c37ca31b66be2e5d218ee5c28544d1e7f
noalotan <noalotan1@gmail.com>
2024-10-27 11:54:30 +0200

    Update eks.tf

--- static/eks.tf
+++ static/eks.tf
@@ -52,6 +52,7 @@ resource "aws_eks_addon" "kube_proxy" {
 resource "aws_eks_addon" "vpc_cni" {
   cluster_name              = module.eks.cluster_name
   addon_name                = "vpc-cni"
+  addon_version             = "v1.18.5-eksbuild.1"  # Added version here
   service_account_role_arn  = "arn:aws:iam::992382545251:role/noa-itay-eks-vpc-cni"
   resolve_conflicts         = "OVERWRITE"
 }
