f688feadb3d930944086167ffe67766f74fa2159
noalotan <noalotan1@gmail.com>
2024-11-02 13:22:32 +0200

    Update dns_change.tf

--- CA/dns_change.tf
+++ CA/dns_change.tf
@@ -1,13 +1,13 @@
 provider "aws" {
   region = "us-east-1"
+}
 
 resource "aws_route53_zone" "my_zone" {
   name = "itay-noa.online"
 }
 
-# Fetch the existing load balancer
 data "aws_lb" "my_load_balancer" {
-  name = "alb-ingress-noa-itay-dev"  
+  name = "alb-ingress-noa-itay-dev"
 }
 
 resource "aws_route53_record" "cname_statuspage" {
@@ -15,13 +15,5 @@ resource "aws_route53_record" "cname_statuspage" {
   name     = "statuspage.itay-noa.online"
   type     = "CNAME"
   ttl      = 300
-  records  = [data.aws_lb.my_load_balancer.dns_name]  
-}
-
-resource "aws_route53_record" "cname_grafana" {
-  zone_id = aws_route53_zone.my_zone.zone_id
-  name     = "grafana.itay-noa.online"
-  type     = "CNAME"
-  ttl      = 300
-  records  = [data.aws_lb.my_load_balancer.dns_name]  
+  records  = [data.aws_lb.my_load_balancer.dns_name]
 }
