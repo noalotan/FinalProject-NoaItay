dc3409493496c097ed23d9fab39b9702d955e61e
noalotan <noalotan1@gmail.com>
2024-11-04 16:37:32 +0200

    Update variables.tf

--- static/variables.tf
+++ static/variables.tf
@@ -9,7 +9,7 @@ variable "region" {
 variable "resource_name" {
   description = "General resource name"
   type        = string
-  default     = "noa-itay-Prod"
+  default     = "noa-itay-prod"
 }
 
 variable "billing_tags" {
